b5c015ebb8174634253632636983cfe6b29b51db
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2022-09-15 21:17:43 +0100

    test: add test for simple elements streaming

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -6,6 +6,8 @@ declare const inlineScriptValues: unknown[];
 
 it("stream-text", fixture(["Hello", "World"]));
 
+it("stream-elements", fixture(["<h1>H1</h1>", "<h2>H2</h2>", "<h3>H3</h3>"]));
+
 it(
   "blocking-scripts",
   fixture([
